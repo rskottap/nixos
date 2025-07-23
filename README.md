99576fb268b14cc97a63ab09344b40f41abb4913
Ramya Kottapalli <rskottap@gmail.com>
2025-07-23 13:19:24 -0500

    Add flake to make packages installable across any OS

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Nix Packages
 
-All my core common packages (devtools, apps, python, fonts etc.,) in one place so I can install them using nix on any OS.
+All my core common packages (devtools, apps, python, fonts etc.,) in one place so I can install them using nix on any OS. See `flake.nix` for included OSes.
+
+After installing nix on the system, run:
+```
+nix profile install .
+```
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Shared system-wide packages for Nix";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          base   = import ./base.nix   { inherit pkgs; };
+          apps   = import ./apps.nix   { inherit pkgs; };
+          fonts  = import ./fonts.nix  { inherit pkgs; };
+          python = import ./python.nix { inherit pkgs; };
+        in {
+          default = pkgs.buildEnv {
+            name = "full-system-packages";
+            paths = base ++ apps ++ fonts ++ python;
+          };
+        }
+      );
+    };
+}
