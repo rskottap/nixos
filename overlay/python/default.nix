71477b5877b116ac9bcd64eadf0351230e7cf572
Ramya Kottapalli <rskottap@gmail.com>
2025-08-22 12:10:07 -0500

    Expose python overlay packages to all available pythons. In base/python.nix select which pythons have which packages. Woot!

--- overlay/python/default.nix
+++ overlay/python/default.nix
@@ -1,14 +1,16 @@
-final: prev: {
+final: prev:
+let
+  pythonOverlays = python-final: python-prev: {
+    lambda-multiprocessing = python-final.callPackage ./lambda-multiprocessing.nix {};
+    callable-module = python-final.callPackage ./callable-module.nix {};
+    is-instance = python-final.callPackage ./is-instance.nix {};
+  };
+in
+{
   python311 = prev.python311.override {
-    packageOverrides = python-final: python-prev: {
-      lambda-multiprocessing = python-final.callPackage ./lambda-multiprocessing.nix {};
-    };
+    packageOverrides = pythonOverlays;
   };
   python313 = prev.python313.override {
-    packageOverrides = python-final: python-prev: {
-      callable-module = python-final.callPackage ./callable-module.nix {};
-      is-instance = python-final.callPackage ./is-instance.nix {};
-    };
+    packageOverrides = pythonOverlays;
   };
-
 }
