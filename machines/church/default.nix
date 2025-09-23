dae327a01f37462c2fc0b008a36fc6cef7b1a4ee
Ramya Kottapalli <rskottap@gmail.com>
2025-09-23 15:12:34 -0700

    Add kdenlive package

--- machines/church/default.nix
+++ machines/church/default.nix
@@ -13,4 +13,8 @@
 
   # Machine-specific configurations can go here
   # For example, if this machine needs specific services or hardware support
+
+  machine.additionalPackages = with pkgs; [
+    kdePackages.kdenlive
+  ];
 }
