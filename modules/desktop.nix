e05413498c1f9d16c660a64439a267e1b784ecab
Ramya Kottapalli <rskottap@gmail.com>
2026-02-19 15:25:24 -0600

    Remap caps to escape!

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,22 +2,23 @@
 
 {
   # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable Lightdm and Cinnamon
-  services.xserver.desktopManager.cinnamon.enable = true;
-
-  services.xserver.displayManager.lightdm = {
+  services.xserver = {
     enable = true;
-  };
+    # Enable Lightdm and Cinnamon
+    desktopManager.cinnamon.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+    displayManager.lightdm = {
+      enable = true;
+    };
+
+    # Configure keymap in X11
+    xkb = {
+      layout = "us";
+      variant = "";
+      options = "caps:escape";
+    };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
+};
 }
